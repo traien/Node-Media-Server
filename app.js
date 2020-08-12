07bd6cc13491a4d0f92e023f07b5f68ef84613f7
OSAMA BASHIR <traien93@gmail.com>
2020-08-12 16:29:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ const config = {
         hls: true,
         hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
         dash: true,
-        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+        mp4: true,
+        mp4Flags: '[movflags=frag_keyframe+empty_moov]',
       }
     ]
   },
